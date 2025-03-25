3ea67b47f2b4d41a68af1c1a2f39bf84269b52e4
Fredwin Rosales <fredwinrosales@gmail.com>
2025-03-25 14:09:04 +0000

    Ajustar respuesta de ejemplo en formato json

--- app/index.ts
+++ app/index.ts
@@ -4,7 +4,11 @@ const app = express();
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).send('OK');
+  res.status(200).json({ status: 'OK' });
+});
+
+app.get('/', (_req, res) => {
+  res.json({ message: '¡Hola desde TypeScript API!' });
 });
 
 app.listen(3000, () => {
